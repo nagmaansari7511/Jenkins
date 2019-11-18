436f2c8b9c39fb1384413b614771be51317b8bfb
Nagma Ansari <nagmaansari@Nagmas-MacBook-Air.local>
2019-11-18 17:04:18 +0100

    added file

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello{
+
+	
+	
+	public static void main(String[] args) {
+		Hello an = new Hello();
+		
+		
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("Hello World");
+			
+		}
+	}
+
+}
